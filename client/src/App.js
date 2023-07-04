87f44c1cd676961cf80fbe234fb36b9dd0316db2
Amy Snarr <amy.snarr16@gmail.com>
2023-07-03 20:46:03 -0600

    added login page and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import Header from './components/header'
 import './styles/App.css';
+import Home from './pages//homePage'
+import Login from './pages/loginPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
     return (
-        <>
-            <Header />
-        </>
+        <Router>
+            <Routes>
+                <Route
+                    path='/'
+                    element={<Home />}
+                />
+                <Route
+                    path='login'
+                    element={<Login />}
+                />
+            </Routes>
+        </Router>
     );
 
 }
