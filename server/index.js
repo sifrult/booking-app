3a507fb3113a41cb2235c71e543ef0a28f8ed318
Amy Snarr <amy.snarr16@gmail.com>
2023-07-24 20:38:21 -0600

    user name remains when reloading page

--- server/index.js
+++ server/index.js
@@ -6,12 +6,13 @@ const app = express();
 require('dotenv').config();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const bcryptSalt = bcrypt.genSaltSync(10)
 const jwtSecret = 'sdfasfwtgafs'
 
 app.use(express.json())
-
+app.use(cookieParser());
 app.use(cors({
   credentials: true,
   origin: 'http://localhost:3000'
@@ -55,6 +56,19 @@ app.post('/login', async (req, res) => {
     }
   } else {
     res.json('not found');
+  }
+});
+
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const {name,email,_id} = await User.findById(userData.id);
+      res.json({name,email,_id});
+    });
+  } else {
+    res.json(null);
   }
 });
 
